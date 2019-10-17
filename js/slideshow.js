28f42f598901cd707e117b631999c99e6b55bb92
rawat.abhishek <abhishek.rawat@vinsol.com>
2019-10-17 17:24:10 +0530

    second commit

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,15 +1,16 @@
 class Slideshow {
-  constructor(slideId, slides, target) {
-    this.slideId = $(slideId);
-    this.slides = this.slideId.find(slides)
+  constructor(selectors) {
+    this.slideId = $(selectors.slideId);
+    this.slides = this.slideId.find(selectors.slidesEle)
     this.slidesLength = this.slides.length;
+    this.slideCounterId = selectors.slideCounterId;
     this.currentSlide = 0;
     this.startShow = this.startShow.bind(this);
-    this.target = $(target);
+    this.target = $(selectors.target);
   }
 
   addSlideCounter() {
-    var counterText = '<h3>total slides: 3</h3><h3>Current Slide: <span id="slideNumber">1</span></h3>'
+    var counterText = '<h3>total slides: 3</h3><h3>Current Slide: <span id="slideCounter">1</span></h3>'
     $(counterText).insertAfter(this.slideId);
   }
 
@@ -22,12 +23,14 @@ class Slideshow {
   }
 
   startShow() {
-    this.slides.eq(this.currentSlide -1).fadeOut('slow', ()=>{
-    if (this.currentSlide === 3) this.currentSlide = 0;
-    this.slides.eq(this.currentSlide).fadeIn('slow');
-    $('#slideNumber').text(this.currentSlide + 1);
-    this.currentSlide++;
-  });
+    this.slides.eq(this.currentSlide -1).fadeOut('slow', () => {
+      if (this.currentSlide === this.slidesLength) {
+        this.currentSlide = 0;
+      }
+      this.slides.eq(this.currentSlide).fadeIn('slow');
+      $('#'+this.slideCounterId).text(this.currentSlide + 1);
+      this.currentSlide++;
+    });
     setTimeout(this.startShow, 3000);
   }
 
@@ -39,4 +42,11 @@ class Slideshow {
   }
 }
 
-(new Slideshow('#slideshow', 'li', '#header')).init();
+selectors = {
+  slideId: '#slideshow',
+  slidesEle: 'li',
+  target: '#header',
+  slideCounterId: 'slideCounter'
+};
+
+$(document).ready((new Slideshow(selectors)).init());
